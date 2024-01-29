0d0432f280849e8349af2dbed8826b4858c67762
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-01-29 23:27:07 +0900

    update

---
+++ frontend/write.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("write-form");
+
+const handleSubmitForm = async (event) => {
+  event.preventDefault();
+  const body = new FormData(form);
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body,
+    });
+    const data = await res.json();
+    if (data === "200") window.location.pathname = "/";
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+form.addEventListener("submit", handleSubmitForm);
