30a64b9811cfd9fb3232e98f3316b5152ab6b645
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-02-03 16:59:10 +0900

    update

---
+++ src/pages/Main.svelte
@@ -0,0 +1,68 @@
+<script>
+    let hour = new Date().getHours();
+    let min = new Date().getMinutes();
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="assets/chart-bar.svg" alt="" />
+      <img src="assets/wifi-bar.svg" alt="" />
+      <img src="assets/battery-bar.svg" alt="" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>정읍시</div>
+      <div class="menu-bar__location-icon">
+        <img src="assets/chevron-down.svg" alt="" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/search.svg" alt="" />
+      <img src="assets/menu.svg" alt="" />
+      <img src="assets/bell.svg" alt="" />
+    </div>
+  </div>
+</header>
+
+<main>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+
+<footer>
+  <div class="footer-block">
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img id="home" src="assets/home.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">홈</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img id="document" src="assets/document.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img id="location" src="assets/location.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img id="chat" src="assets/chat.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img id="user" src="assets/user.svg" alt="" />
+      </div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </div>
+  </div>
+</footer>
+<div class="media-info-msg">화면 사이즈를 줄여주세요.</div>
