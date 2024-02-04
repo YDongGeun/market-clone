15b33f80793a0c9d53518ad59055fc4a4382d70e
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-02-04 19:52:55 +0900

    유저 인증까지 파이어베이스로 구현

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,47 @@
   import Write from "./pages/Write.svelte";
   import Router from "svelte-spa-router";
   import NotFound from "./pages/NotFound.svelte";
+  import MyPage from "./pages/MyPage.svelte";
   import "./css/style.css";
+  import { user$ } from "./store";
+  import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+
+  let isLoading = true;
+
+  const auth = getAuth();
+
+  const chkchLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
-    '/':Main,
-    '/login':Login,
-    '/signup':Signup,
-    '/write':Write,
-    "*":NotFound
-  }
+    "/": Main,
+    "/signup": Signup,
+    "/write": Write,
+    "/my": MyPage,
+    "*": NotFound,
+  };
+
+  onMount(() => chkchLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// @ts-ignore
 import App from './App.svelte'
+import "../firebase.js";
 
 const app = new App({
   target: document.getElementById('app'),
